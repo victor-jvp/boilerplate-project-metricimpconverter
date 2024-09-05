df2b57c8128bd4885e022fc8f1e2aec1250d278a
Victor Veliz <ing.vj15@gmail.com>
2024-09-05 17:42:02 -0400

    Added missing validations

--- controllers/convertHandler.js
+++ controllers/convertHandler.js
@@ -1,44 +1,75 @@
 function ConvertHandler() {
-  
-  this.getNum = function(input) {
-    let result;
-    
-    return result;
+
+  const units = {
+    gal: "L",
+    L: "gal",
+    mi: "km",
+    km: "mi",
+    lbs: "kg",
+    kg: "lbs",
   };
-  
-  this.getUnit = function(input) {
-    let result;
-    
-    return result;
-  };
-  
-  this.getReturnUnit = function(initUnit) {
-    let result;
-    
+
+  this.getNum = function (input) {
+    let unit = input.match(/[a-zA-Z]+/g)
+    if (unit.length == 1) unit = unit[0]
+    else unit = unit.join();
+    let result = input.replace(unit, "")
+    if (result.indexOf('/') !== -1) {
+      num = result.split('/')
+      result = num[0] / num[1]
+    }
+    if (!isFinite(result)) return false;
     return result;
   };
 
-  this.spellOutUnit = function(unit) {
-    let result;
-    
-    return result;
+  this.getUnit = function (input) {
+    let unit = input.match(/[a-zA-Z]+/g)
+    if (unit.length == 1) unit = unit[0]
+    else unit = unit.join();
+    return this.spellOutUnit(unit);
   };
-  
-  this.convert = function(initNum, initUnit) {
+
+  this.getReturnUnit = function (initUnit) {
+    return units[initUnit];
+  };
+
+  this.spellOutUnit = function (unit) {
+    if (unit === "L" || unit === "l") return "L";
+    if (!units.hasOwnProperty(unit.toLowerCase())) return false;
+    return unit.toLowerCase();
+  };
+
+  this.convert = function (initNum, initUnit) {
     const galToL = 3.78541;
     const lbsToKg = 0.453592;
     const miToKm = 1.60934;
-    let result;
-    
-    return result;
+
+    const convertRate = {
+      gal: galToL,
+      L: 1 / galToL,
+      mi: miToKm,
+      km: 1 / miToKm,
+      lbs: lbsToKg,
+      kg: 1 / lbsToKg,
+    }
+
+    return Math.round(convertRate[initUnit] * initNum * 100000) / 100000;
   };
-  
-  this.getString = function(initNum, initUnit, returnNum, returnUnit) {
-    let result;
-    
-    return result;
+
+  this.getString = function (initNum, initUnit, returnNum, returnUnit) {
+
+    const unitLong = {
+      gal: "gallons",
+      L: "liters",
+      mi: "miles",
+      km: "kilometers",
+      lbs: "pounds",
+      kg: "kilograms",
+    }
+
+    return `${initNum} ${unitLong[initUnit]} converts to ${returnNum} ${unitLong[returnUnit]}`;
   };
-  
+
 }
 
 module.exports = ConvertHandler;
--- routes/api.js
+++ routes/api.js
@@ -4,7 +4,31 @@ const expect = require('chai').expect;
 const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
-  
-  let convertHandler = new ConvertHandler();
 
+
+  app.get('/api/convert', (req, res) => {
+    let convertHandler = new ConvertHandler();
+    const input = req.query.input
+    if(!input) return res.json("invalid unit")
+    
+    const initNum = convertHandler.getNum(input)
+    const initUnit = convertHandler.getUnit(input)
+
+    if (!initNum || !initUnit) {
+      if (!initNum && !initUnit) return res.json('invalid number and unit');
+      if (!initNum) return res.json('invalid number');
+      if (!initUnit) return res.json('invalid unit');
+    }
+
+    const returnNum = convertHandler.convert(initNum, initUnit)
+    const returnUnit = convertHandler.getReturnUnit(initUnit)
+    const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+    res.json({
+      initNum,
+      initUnit,
+      returnNum,
+      returnUnit,
+      string
+    })
+  })
 };
