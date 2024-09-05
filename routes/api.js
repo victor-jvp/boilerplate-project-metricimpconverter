5804cd9683c67597ec1658077072ab0965f8c226
Victor Véliz <ing.vj15@gmail.com>
2024-09-05 18:38:43 +0000

    Update test with get endpoint and calcs

--- routes/api.js
+++ routes/api.js
@@ -5,6 +5,27 @@ const ConvertHandler = require('../controllers/convertHandler.js');
 
 module.exports = function (app) {
   
-  let convertHandler = new ConvertHandler();
+
+  app.get('/api/convert', (req, res) => {
+    try {
+      let convertHandler = new ConvertHandler();
+      const input = req.query.input
+      const initNum = convertHandler.getNum(input)
+      const initUnit = convertHandler.getUnit(input)
+      const returnNum = convertHandler.convert(initNum, initUnit)
+      const returnUnit = convertHandler.getReturnUnit(initUnit)
+      const string = convertHandler.getString(initNum, initUnit, returnNum, returnUnit)
+      res.json({ 
+        initNum,
+        initUnit,
+        returnNum,
+        returnUnit,
+        string
+       })
+    } catch (error) {
+      res.json(error.message)
+    }
+
+  })
 
 };
